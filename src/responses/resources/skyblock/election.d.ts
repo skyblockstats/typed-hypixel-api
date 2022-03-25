1d4d83fb5e08995db5002c2a83ca18f60c49308e
mat <github@matdoes.dev>
2022-03-25 19:01:16 +0000

    add release action

---
+++ src/responses/resources/skyblock/election.d.ts
@@ -0,0 +1,27 @@
+interface Candidate {
+    key: string
+    name: string
+    perks: {
+        name: string
+        description: string
+    }[]
+}
+
+export interface SkyBlockElectionResponse {
+    success: true
+    lastUpdated: number
+    mayor: Candidate & {
+        election: {
+            year: number
+            candidates: (Candidate & {
+                votes: number
+            })[]
+        }
+    }
+    current?: {
+        year: number
+        candidates: (Candidate & {
+            votes: number
+        })[]
+    }
+}
